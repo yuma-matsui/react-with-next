3f6bd95b7b33a5216c58bad1c04323dd9f2cacf6
yuma-matsui <oshino.iiyo@gmail.com>
2023-07-12 06:31:55 +0900

    ⚡️ useReducerを使った

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { FC, useCallback, useEffect, useState } from "react";
+import React, { FC, Reducer, useCallback, useEffect, useReducer } from "react";
 
 type Post = {
   userId: number;
@@ -8,19 +7,52 @@ type Post = {
   body: string;
 };
 
+type PostsState = {
+  posts: Post[];
+  loading: boolean;
+  error: Error | null;
+};
+
+type Action =
+  | {
+      type: "posts";
+      payload: {
+        value: PostsState["posts"];
+      };
+    }
+  | {
+      type: "error";
+      payload: {
+        value: PostsState["error"];
+      };
+    };
+
+const postsReducer: Reducer<PostsState, Action> = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "posts":
+      return { ...state, posts: payload.value, loading: false };
+    case "error":
+      return { ...state, error: payload.value, loading: false };
+    default:
+      throw new Error("No such action type.");
+  }
+};
+
 const Posts: FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
+  const [postsState, dispatch] = useReducer(postsReducer, {
+    posts: [],
+    loading: true,
+    error: null,
+  });
+
   const getPosts = useCallback(async () => {
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
       const json = await res.json();
-      setPosts(json);
-    } catch (e) {
-      if (e instanceof Error) setError(e);
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "posts", payload: { value: json } });
+    } catch (error) {
+      if (error instanceof Error) dispatch({ type: "error", payload: { value: error } });
     }
   }, []);
 
@@ -28,13 +60,15 @@ const Posts: FC = () => {
     getPosts();
   }, [getPosts]);
 
-  if (isLoading) return <p>Loading....</p>;
-  if (error) return <p>Something went wrong!</p>;
-  if (posts.length <= 0) return <p>データは空です</p>;
+  console.log("foo");
+
+  if (postsState.loading) return <p>Loading....</p>;
+  if (postsState.error) return <p>Something went wrong!</p>;
+  if (postsState.posts.length <= 0) return <p>データは空です</p>;
 
   return (
     <ol>
-      {posts.map((post) => (
+      {postsState.posts.map((post) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ol>
